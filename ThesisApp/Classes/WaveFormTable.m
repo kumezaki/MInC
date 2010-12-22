b76ff70ea185424f250f39ec59f1e91e2318c32f
Chris Lavender <emaillavender@gmail.com>
2010-12-22 00:06:21 +0000

    incremental work on waveforms

--- ThesisApp/Classes/WaveFormTable.m
+++ ThesisApp/Classes/WaveFormTable.m
@@ -18,30 +18,39 @@
 	return self;
 }
 
+#define SIGN(x)	(x < 0. ? -1. : x > 0. ? +1. : 0.)
+
 -(void) createWaveType:(NSString *)waveType
 {
 	mWaveType = waveType;
 	if ([mWaveType isEqual:@"sine wave"]) {
 		for (int i = 0; i < kWaveformTableSize; i++)
 		{
-			mTable[i] = sin((double)i / kWaveformTableSize * 2. * 3.1415926353);
+			mTable[i] = sinf((double)i / kWaveformTableSize * 2. * M_PI);
 		}
 	}
 	else if ([mWaveType isEqual:@"square wave"]){
 		for (int i = 0; i < kWaveformTableSize; i++)
 		{
-			if (i < (kWaveformTableSize / 2)) mTable[i] = 1;
-			else mTable[i] = -1;
+			double mTheta = (double)i / kWaveformTableSize;
+			mTable[i] = SIGN(sinf(mTheta * 2 * M_PI));
 		}
 	}
 	else if ([mWaveType isEqual:@"sawtooth wave"]){
 		for (int i = 0; i < kWaveformTableSize; i++)
 		{
 			double mTheta = (double)i / kWaveformTableSize;
-			mTable[i] = mTheta - floor(mTheta + 0.5);
+			mTable[i] = 2 * (mTheta - floor(mTheta + 0.5));
 		}
-	}		
-	
+	}
+	else if ([mWaveType isEqual:@"triangle wave"]){
+		for (int i = 0; i < kWaveformTableSize; i++)
+		{
+			double mTheta = (double)i / kWaveformTableSize;
+			if (i < (kWaveformTableSize / 2)) mTable[i] = 2 * (mTheta - floor(mTheta + 0.5));
+			else mTable[i] = -2 * (mTheta - floor(mTheta + 0.5));
+		}
+	}
 }
 
 -(double) get:(double)index
