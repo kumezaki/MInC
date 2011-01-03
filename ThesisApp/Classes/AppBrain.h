b5387878de74ca78ba24cd53ac3ff5348919bec1
Chris Lavender <emaillavender@gmail.com>
2011-01-03 22:41:46 +0000

    forgot to add new class

---
+++ ThesisApp/Classes/AppBrain.h
@@ -0,0 +1,44 @@
+//
+//  AppBrain.h
+//  ThesisApp
+//
+//  Created by Chris Lavender on 1/3/11.
+//  Copyright 2011 Gnarly Dog Music. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#import "AQPlayer.h"
+#import "Envelope.h"
+#import "Mode.h"
+#import "Note.h"
+#import "WaveFormTable.h"
+
+@interface AppBrain : AQPlayer {
+
+@private
+	
+	NSString *mWaveType;
+	
+	double mFreq;
+	int mCurrentMode;
+	
+@public
+	
+	Mode *mModes[kNumberModes];
+	Note *mNotes[kNumberNotes];
+	WaveFormTable *mWaveTable;		
+}
+
+@property int mCurrentMode;
+@property (nonatomic, retain) NSString *mWaveType;
+
+-(void) startNote:(int)note_pos;
+-(void) stopNote:(int)note_pos;
+
+-(void) setMode:(int)val;
+-(void) setWaveType:(NSString *)waveType;
+
+- (void)fillAudioBuffer:(double*)buffer:(UInt32)numFrames;
+
+@end
