265c86b57a154befa9ad216f2030b7da254acbba
Chris Lavender <emaillavender@gmail.com>
2011-02-05 08:09:46 +0000

    First attempt at networking. Having trouble implementing a global networking object.

--- Thumbafon/Classes/Networking.m
+++ Thumbafon/Classes/Networking.m
@@ -25,8 +25,7 @@ Networking *gNetwork = nil;
 
 @implementation Networking
 
--(id)init
-{
+-(id)init {
 	[super init];
 	
 	mSendIPAddress = 0xa000112;	//0x7F000001; /* IP address: 127.0.0.1 */
@@ -40,8 +39,7 @@ Networking *gNetwork = nil;
 	return self;
 }
 
-- (NSString *)getIPAddress
-{
+- (NSString *)getIPAddress {
 	NSString *address = @"0.0.0.0";
 	struct ifaddrs *interfaces = NULL;
 	struct ifaddrs *temp_addr = NULL;
@@ -49,17 +47,16 @@ Networking *gNetwork = nil;
 	
 	// retrieve the current interfaces - returns 0 on success
 	success = getifaddrs(&interfaces);
-	if (success == 0)
-	{
+	if (success == 0) {
+		
 		// Loop through linked list of interfaces
 		temp_addr = interfaces;
-		while (temp_addr != NULL)
-		{
-			if (temp_addr->ifa_addr->sa_family == AF_INET)
-			{
+		while (temp_addr != NULL) {
+			
+			if (temp_addr->ifa_addr->sa_family == AF_INET) {
+				
 				// Check if interface is en0 which is the wifi connection on the iPhone
-				if ([[NSString stringWithUTF8String:temp_addr->ifa_name] isEqualToString:@"en0"])	/* on device it's en0, on simulator it's en1 */
-				{
+				if ([[NSString stringWithUTF8String:temp_addr->ifa_name] isEqualToString:@"en0"]) {	/* on device it's en0, on simulator it's en1 */
 					// Get NSString from C String
 					address = [NSString stringWithUTF8String:inet_ntoa(((struct sockaddr_in *)temp_addr->ifa_addr)->sin_addr)];
 				}
@@ -73,15 +70,15 @@ Networking *gNetwork = nil;
 	return address;
 }
 
--(void)sendUDP
-{
+-(void)sendUDP {
+	
 	int sock;
 	struct sockaddr_in sa;
 	int bytes_sent;
 	
 	sock = socket(PF_INET, SOCK_DGRAM, IPPROTO_UDP);
-	if (-1 == sock) /* if socket failed to initialize, exit */
-    {
+	if (-1 == sock) /* if socket failed to initialize, exit */ {
+		
 		fprintf(stderr,"Error creating socket: %s\n",strerror(errno));
 		exit(EXIT_FAILURE);
     }
@@ -98,8 +95,8 @@ Networking *gNetwork = nil;
 	close(sock); /* close the socket */
 }
 
--(void)sendOSCMsg:(const char*)osc_str:(int)osc_str_length
-{
+-(void)sendOSCMsg:(const char*)osc_str:(int)osc_str_length {
+	
 	char buf[1024]; memcpy(buf,osc_str,osc_str_length); memcpy(buf+osc_str_length,",s\0\0",4);
 	
 	OSC_START
@@ -108,8 +105,8 @@ Networking *gNetwork = nil;
 	OSC_END
 }
 
--(void)sendOSCMsgWithIntValue:(const char*)osc_str:(int)osc_str_length:(int)val
-{
+-(void)sendOSCMsgWithIntValue:(const char*)osc_str:(int)osc_str_length:(int)val {
+	
 	char buf[128]; memcpy(buf,osc_str,osc_str_length); memcpy(buf+osc_str_length,",si\0",4);
 	val = htonl(val);
 	
@@ -120,8 +117,8 @@ Networking *gNetwork = nil;
 	OSC_END
 }
 
--(void)sendOSCMsgWith2IntValues:(const char*)osc_str:(int)osc_str_length:(int)val0:(int)val1
-{
+-(void)sendOSCMsgWith2IntValues:(const char*)osc_str:(int)osc_str_length:(int)val0:(int)val1 {
+	
 	char buf[128]; memcpy(buf,osc_str,osc_str_length); memcpy(buf+osc_str_length,",sii\0\0\0\0",8);
 	val0 = htonl(val0);
 	val1 = htonl(val1);
