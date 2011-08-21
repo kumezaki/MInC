264a5ad90217b00bfe5423536626c2e194a16920
Ko Umezaki <ko@healthyboys.com>
2011-08-21 14:53:23 +0000

    added scripting and additional program to handle sending raw data from server to iOS device

---
+++ Sagariha/sample_tcp_code/fz_download_script0.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+DONE=0
+FILE_NAME=./fz_download.txt
+
+# this is for debugging only
+cp ./download.txt ./fz_download.txt
+#cp ./download_exit.txt ./fz_download.txt
+
+while [ $DONE = 0 ]; do
+	if [ -e $FILE_NAME ]; then
+		awk -f fz_download_awk0.txt $FILE_NAME
+		if [ $? = 1 ]; then
+			echo exiting fz_download_script0.sh
+			DONE=1
+		fi
+		rm $FILE_NAME
+	else
+# this is for debugging only
+		echo $FILE_NAME not found
+	fi
+	if [ $DONE = 0 ]; then
+		sleep 0.5
+	fi
+done
+
+exit
