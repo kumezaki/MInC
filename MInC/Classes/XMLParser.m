e0ca80dfffd6c54f9c7ae43aff143216e7028704
Philip Marazita <pmarazit@uci.edu>
2012-02-08 23:14:24 +0000

    Added xmlparser .h and .m files

---
+++ MInC/Classes/XMLParser.m
@@ -0,0 +1,44 @@
+//
+//  XMLParser.m
+//  MInC
+//
+//  Created by Philip Marazita on 2/8/12.
+//  Copyright 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "XMLParser.h"
+
+#import <libxml/tree.h>
+#import <libxml/parser.h>
+#import <libxml/HTMLparser.h>
+#import <libxml/xpath.h>
+#import <libxml/xpathInternals.h>
+
+@implementation XMLParser
+
+
+- (id)initWithFrame:(CGRect)frame {
+    
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code.
+    }
+    return self;
+}
+
+
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code.
+}
+*/
+
+- (void)dealloc {
+    [super dealloc];
+}
+
+
+@end
